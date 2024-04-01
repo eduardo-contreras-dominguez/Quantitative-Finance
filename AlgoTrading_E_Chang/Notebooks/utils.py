50cf445e63ab0d6e02e0a549217d40f8850bdb68
educontreras <edu.contreras@gmail.com>
2024-04-01 19:33:05 +0200

    added in utils the possibility to retrieve hourly data, only last 730 days available

--- AlgoTrading_E_Chang/Notebooks/utils.py
+++ AlgoTrading_E_Chang/Notebooks/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fb8ea72785cccd8f3bced7b3b395a89a7eec0bfe7b0408ec575200449f38c51
-size 772
+oid sha256:3f3e0d75d314ef4c7fa1d967b01e45aa24ad96f746d79fc6130f4d87f64a0147
+size 1974
