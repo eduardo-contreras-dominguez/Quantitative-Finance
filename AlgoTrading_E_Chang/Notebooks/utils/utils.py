628f442f7bfd0d076af019cedcab88d874b92099
educontreras <edu.contreras@gmail.com>
2024-04-04 10:50:56 +0200

    Momentum Strategy. Most basic one seeming to work

--- AlgoTrading_E_Chang/Notebooks/utils/utils.py
+++ AlgoTrading_E_Chang/Notebooks/utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06f23dbe9059fbf1dffff19b196d03da6a5bbe98724786753451329059923184
-size 3674
+oid sha256:62fd6deb1f4b84e45b30e63502c9c216edb9564342600e571af22a2922b0a75b
+size 3625
---
+++ AlgoTrading_E_Chang/Working_Strategies/Momentum.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3f723a6cb08cc4facfb4ec5c0402a91648b0440f8b5028d22612c39bb718a4c
+size 2673
