2005473e3c54c0c2662d0462319f6c97e51312dc
educontreras <edu.contreras@gmail.com>
2024-04-12 09:15:18 +0200

    Created multi spectral RSI indicator. Based on average divergence in short term and long term RSI crossover.

---
+++ Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:491c216432ffcddf1e0e719b5d535fa91dc5dcb2b049ce7d918bdf17c387071e
+size 4244
---
+++ Indicators/Momentum/momentum_computing_utils.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be33ed900613e912796060e381e1181cc3fd47a8321ec4fe21c3a862e8375cd6
+size 1508
