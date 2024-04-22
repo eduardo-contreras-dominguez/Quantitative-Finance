5d4d174682185e7c89908b39c7aa85791b47f19c
educontreras <edu.contreras@gmail.com>
2024-04-22 09:20:31 +0200

    New strategy with signal on open hour and entering the trade at closing price of this day, avoiding to shift(2) the trade

--- Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
+++ Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0af0a4769f4f76b22e5892e49dc47381650d00a1f349fc6b45261fc857c084d4
-size 10951
+oid sha256:bf64c1ba2cb7faaf847a7fca6644db75795ac48773e370660fc56d166831c396
+size 11614
