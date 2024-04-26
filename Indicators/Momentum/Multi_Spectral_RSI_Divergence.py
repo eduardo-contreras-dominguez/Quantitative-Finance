9ae155e20777a0b4a143ed0fbcc1062945f7a522
educontreras <edu.contreras@gmail.com>
2024-04-26 10:27:57 +0200

    Changed an error in the number of days trend at the end of the signal dataframe

--- Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
+++ Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf64c1ba2cb7faaf847a7fca6644db75795ac48773e370660fc56d166831c396
-size 11614
+oid sha256:e256da9839d6727ccf070087e785970c70c9f64396fed9b0fda6b05e04fba25e
+size 11889
