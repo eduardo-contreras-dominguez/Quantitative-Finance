a06168c9ec465c6b147d588da1cc0d137aefa97c
educontreras <edu.contreras@gmail.com>
2024-05-06 08:28:19 +0200

    New changes, started to introduce a method that will give us a summary on a given strategy (all information we need, Sharpe Ratio, Sample size, hit ratios, etc.)

--- Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
+++ Indicators/Momentum/Multi_Spectral_RSI_Divergence.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e256da9839d6727ccf070087e785970c70c9f64396fed9b0fda6b05e04fba25e
-size 11889
+oid sha256:4a51f626c0ff9a451a451d4d9bf4d8793ce92ce145c852669c62ac812d79f6e3
+size 13362
---
+++ Indicators/strategy_utils.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3eb386004ce90cd71c2552533b6160a255ac90598abdbbf3aaea3ad1ae9ea9ef
+size 1183
